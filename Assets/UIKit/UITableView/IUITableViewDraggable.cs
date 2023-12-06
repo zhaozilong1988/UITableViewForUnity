2d3f2dee9a694a4a8904f14554d555cbf6ffabf7
zhaozilong1988 <zhaozilong0906@gmail.com>
2023-12-07 08:26:18 +0900

    Refactor IUITableViewDraggable

--- Assets/UIKit/UITableView/IUITableViewDraggable.cs
+++ Assets/UIKit/UITableView/IUITableViewDraggable.cs
@@ -8,16 +8,20 @@ namespace UIKit
 	public interface IUITableViewDraggable : IUITableViewInteractable
 	{
 		/// <summary>
+		/// The cell will be dragged to move position if this returns true.
+		/// </summary>
+		bool TableViewDragCellMovable(UITableView tableView);
+		/// <summary>
 		/// Called on the drag UITableViewCell at index when dragging is about to begin
 		/// </summary>
-		bool TableViewOnBeginDragCellAt(UITableView tableView, int draggedIndex, PointerEventData eventData);
+		void TableViewOnBeginDrag(UITableView tableView, int? draggedIndex, PointerEventData eventData);
 		/// <summary>
 		/// Called on the drag UITableViewCell at index when a drag is happening
 		/// </summary>
-		void TableViewOnDragCellAt(UITableView tableView, int draggedIndex, PointerEventData eventData);
+		void TableViewOnDrag(UITableView tableView, int? draggedIndex, PointerEventData eventData);
 		/// <summary>
 		/// Called on the drag UITableViewCell at index when a drag finishes
 		/// </summary>
-		void TableViewOnEndDragCellAt(UITableView tableView, int draggedIndex, PointerEventData eventData);
+		void TableViewOnEndDrag(UITableView tableView, int? draggedIndex, PointerEventData eventData);
 	}
 }
