0a39213ce08375dbf1b242e70e6ae4220bc2f90c
Zhao Zilong <zhaozilong0906@gmail.com>
2024-12-01 11:19:26 +0900

    sample: chat scene

---
+++ Assets/UIKit/Samples/1_Chat/Scripts/ChatCell.cs
@@ -0,0 +1,22 @@
+﻿using UIKit;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatCell : UITableViewCell
+{
+	[SerializeField] Image _bg;
+	[SerializeField] Text _text;
+
+	public void UpdateData(string text, int fontSize, float bubbleWidth, float bubbleHeight)
+	{
+		_text.text = text;
+		_text.fontSize = fontSize;
+		const float minimumWidth = 150f;
+		_bg.rectTransform.sizeDelta = new Vector2(Mathf.Max(bubbleWidth, minimumWidth), bubbleHeight);
+	}
+
+	public void UnloadData()
+	{
+		_text.text = string.Empty;
+	}
+}
